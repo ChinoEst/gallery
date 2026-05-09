7ee6b8c4b320afd1eb9b60651e6084308862e259
Wayne <a27689259@gmail.com>
2026-05-09 09:47:35 +0800

    add note on routers/auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -12,35 +12,57 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 
 
-
+#a tool for pick up token from request
+#get this: headers = {"Authorization": f"Bearer {token}"}
 security = HTTPBearer()
+
 DEV_MODE = False
+
+#for app.include_router(auth.router)
 router = APIRouter(prefix="/auth", tags=["auth"])
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 
 
+#lock
 def hash_password(password: str):
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
+
+#verify
 def verify_password(plain: str, hashed: str):
     return bcrypt.checkpw(plain.encode(), hashed.encode())
 
+
+#create token for who connects
 def create_token(username: str, role: str):
     expire = datetime.utcnow() + timedelta(hours=24)
     data = {"sub": username, "role": role, "exp": expire}
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
+
+#register new account
+#router: router = APIRouter(prefix="/auth", tags=["auth"])
+#real path: routers/auth/register  <-call this,   not def register
+#response_model: restrict output format
 @router.post("/register", response_model=TokenResponse)
 async def register(body: UserRegister, db: AsyncSession = Depends(get_db)):
+    #check whether user exist by SQL
     result = await db.execute(select(User).where(User.username == body.username))
     if result.scalar_one_or_none():
         raise HTTPException(status_code=400, detail="帳號已存在")
+    
+    #create new user 
     user = User(username=body.username, password=hash_password(body.password))
+
+    #add new user to SQLAlchemy by models.user : __tablename__ = "users"
     db.add(user)
     await db.commit()
+
+    #give token who registers
     token = create_token(user.username, user.role)
     return {"token": token}
+
 
 @router.post("/login", response_model=TokenResponse)
 async def login(body: UserLogin, db: AsyncSession = Depends(get_db)):
