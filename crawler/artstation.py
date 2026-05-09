fee58f3789f00f73c86be58b8d6406a5c1b824a3
Wayne <a27689259@gmail.com>
2026-05-09 20:58:38 +0800

    add note on crawler.py

--- crawler/artstation.py
+++ crawler/artstation.py
@@ -18,28 +18,43 @@ async def get_or_create_tag(db: DBSession, tag_name: str) -> Tag:
 async def crawl_project(client, hash_id: str, artist: Artist, db: DBSession):
     async with CRAWL_SEMAPHORE:
         try:
+            #get url from site for database
             res = await client.get(f"https://www.artstation.com/projects/{hash_id}.json")
             data = res.json()
 
+            #access all tag with all images from  artist
             tags = [c["name"] for c in data.get("categories", [])]
+
+            #cover as Thumbnail
             cover_url = data.get("cover_url")
 
             for asset in data.get("assets", []):
                 if not asset.get("has_image"):
                     continue
-
+                
                 image_url = asset.get("image_url")
 
                 result = await db.execute(select(Image).where(Image.original_url == image_url))
                 if result.scalar_one_or_none():
                     continue
-
+                    
                 image = Image(
                     artist_id=artist.id,
                     original_url=image_url,
                     thumbnail_url=cover_url,
                 )
+
+                
                 db.add(image)
+                """
+                   add   -> flush -> commit -> refresh
+                register    send     updata     sync
+                
+
+                flush  vs refresh
+                
+
+                """
                 await db.flush()
 
                 for tag_name in tags:
@@ -49,13 +64,16 @@ async def crawl_project(client, hash_id: str, artist: Artist, db: DBSession):
             await db.commit()
 
         except Exception as e:
+            #restore all changes if crawl failed
             await db.rollback()
             print(f"爬取失敗 hash_id={hash_id}: {e}")
 
-async def crawl_artstation(artist: Artist, db: AsyncSession):
+async def crawl_artstation(artist: Artist, db: AsyncSession):##
     username = artist.url.rstrip("/").split("/")[-1]
     artist_name = artist.name
     print(f"開始爬取 ArtStation: {username}")
+
+    #pretend a browser to avoid being blocked
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
     async with AsyncSession(impersonate="chrome") as client:
         res = await client.get(f"https://www.artstation.com/users/{username}/projects.json")
