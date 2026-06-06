d0d419b22ab339cc2dff73a3b3e1c1320ff0b433
Wayne <a27689259@gmail.com>
2026-06-06 17:24:15 +0800

    url

--- pytest.py
+++ pytest.py
@@ -10,6 +10,7 @@ def test_2():
     assert "hello" + " world" == "hello world"
 
 def test_登入():
+    BASE_URL = "http://127.0.0.1:8000"
     res = httpx.post(f"{BASE_URL}/auth/login", json={"username": "admin", "password": "1234"})
     assert res.status_code == 200
     assert "token" in res.json()
